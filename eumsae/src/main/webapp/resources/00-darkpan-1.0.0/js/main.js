0c4dcf81586a6b1cbf63bf7654b21e07d2ac1a0b
Choe Gitae <cgtcsg@naver.com>
2022-11-15 18:44:46 +0900

    관리페이지 LP등록페이지 front 작업

--- eumsae/src/main/webapp/resources/00-darkpan-1.0.0/js/main.js
+++ eumsae/src/main/webapp/resources/00-darkpan-1.0.0/js/main.js
@@ -1,52 +1,54 @@
+$(function () {
+    $("img.sidebar-logo").css({ width: "30px", height: "30px", margin: "0px 10px 5px 0px" });
+});
+
 (function ($) {
     "use strict";
 
     // Spinner
     var spinner = function () {
         setTimeout(function () {
-            if ($('#spinner').length > 0) {
-                $('#spinner').removeClass('show');
+            if ($("#spinner").length > 0) {
+                $("#spinner").removeClass("show");
             }
         }, 1);
     };
     spinner();
-    
-    
+
     // Back to top button
     $(window).scroll(function () {
         if ($(this).scrollTop() > 300) {
-            $('.back-to-top').fadeIn('slow');
+            $(".back-to-top").fadeIn("slow");
         } else {
-            $('.back-to-top').fadeOut('slow');
+            $(".back-to-top").fadeOut("slow");
         }
     });
-    $('.back-to-top').click(function () {
-        $('html, body').animate({scrollTop: 0}, 1500, 'easeInOutExpo');
+    $(".back-to-top").click(function () {
+        $("html, body").animate({ scrollTop: 0 }, 1500, "easeInOutExpo");
         return false;
     });
-
 
     // Sidebar Toggler
-    $('.sidebar-toggler').click(function () {
-        $('.sidebar, .content').toggleClass("open");
+    $(".sidebar-toggler").click(function () {
+        $(".sidebar, .content").toggleClass("open");
         return false;
     });
 
-
     // Progress Bar
-    $('.pg-bar').waypoint(function () {
-        $('.progress .progress-bar').each(function () {
-            $(this).css("width", $(this).attr("aria-valuenow") + '%');
-        });
-    }, {offset: '80%'});
-
+    $(".pg-bar").waypoint(
+        function () {
+            $(".progress .progress-bar").each(function () {
+                $(this).css("width", $(this).attr("aria-valuenow") + "%");
+            });
+        },
+        { offset: "80%" }
+    );
 
     // Calender
-    $('#calender').datetimepicker({
+    $("#calender").datetimepicker({
         inline: true,
-        format: 'L'
+        format: "L",
     });
-
 
     // Testimonials carousel
     $(".testimonial-carousel").owlCarousel({
@@ -55,14 +57,12 @@
         items: 1,
         dots: true,
         loop: true,
-        nav : false
+        nav: false,
     });
-
 
     // Chart Global Color
     Chart.defaults.color = "#6C7293";
     Chart.defaults.borderColor = "#000000";
-
 
     // Worldwide Sales Chart
     var ctx1 = $("#worldwide-sales").get(0).getContext("2d");
@@ -70,28 +70,28 @@
         type: "bar",
         data: {
             labels: ["2016", "2017", "2018", "2019", "2020", "2021", "2022"],
-            datasets: [{
+            datasets: [
+                {
                     label: "USA",
                     data: [15, 30, 55, 65, 60, 80, 95],
-                    backgroundColor: "rgba(235, 22, 22, .7)"
+                    backgroundColor: "rgba(235, 22, 22, .7)",
                 },
                 {
                     label: "UK",
                     data: [8, 35, 40, 60, 70, 55, 75],
-                    backgroundColor: "rgba(235, 22, 22, .5)"
+                    backgroundColor: "rgba(235, 22, 22, .5)",
                 },
                 {
                     label: "AU",
                     data: [12, 25, 45, 55, 65, 70, 60],
-                    backgroundColor: "rgba(235, 22, 22, .3)"
-                }
-            ]
-            },
+                    backgroundColor: "rgba(235, 22, 22, .3)",
+                },
+            ],
+        },
         options: {
-            responsive: true
-        }
+            responsive: true,
+        },
     });
-
 
     // Salse & Revenue Chart
     var ctx2 = $("#salse-revenue").get(0).getContext("2d");
@@ -99,26 +99,25 @@
         type: "line",
         data: {
             labels: ["2016", "2017", "2018", "2019", "2020", "2021", "2022"],
-            datasets: [{
+            datasets: [
+                {
                     label: "Salse",
                     data: [15, 30, 55, 45, 70, 65, 85],
                     backgroundColor: "rgba(235, 22, 22, .7)",
-                    fill: true
+                    fill: true,
                 },
                 {
                     label: "Revenue",
                     data: [99, 135, 170, 130, 190, 180, 270],
                     backgroundColor: "rgba(235, 22, 22, .5)",
-                    fill: true
-                }
-            ]
-            },
+                    fill: true,
+                },
+            ],
+        },
         options: {
-            responsive: true
-        }
+            responsive: true,
+        },
     });
-    
-
 
     // Single Line Chart
     var ctx3 = $("#line-chart").get(0).getContext("2d");
@@ -126,18 +125,19 @@
         type: "line",
         data: {
             labels: [50, 60, 70, 80, 90, 100, 110, 120, 130, 140, 150],
-            datasets: [{
-                label: "Salse",
-                fill: false,
-                backgroundColor: "rgba(235, 22, 22, .7)",
-                data: [7, 8, 8, 9, 9, 9, 10, 11, 14, 14, 15]
-            }]
+            datasets: [
+                {
+                    label: "Salse",
+                    fill: false,
+                    backgroundColor: "rgba(235, 22, 22, .7)",
+                    data: [7, 8, 8, 9, 9, 9, 10, 11, 14, 14, 15],
+                },
+            ],
         },
         options: {
-            responsive: true
-        }
+            responsive: true,
+        },
     });
-
 
     // Single Bar Chart
     var ctx4 = $("#bar-chart").get(0).getContext("2d");
@@ -145,22 +145,17 @@
         type: "bar",
         data: {
             labels: ["Italy", "France", "Spain", "USA", "Argentina"],
-            datasets: [{
-                backgroundColor: [
-                    "rgba(235, 22, 22, .7)",
-                    "rgba(235, 22, 22, .6)",
-                    "rgba(235, 22, 22, .5)",
-                    "rgba(235, 22, 22, .4)",
-                    "rgba(235, 22, 22, .3)"
-                ],
-                data: [55, 49, 44, 24, 15]
-            }]
+            datasets: [
+                {
+                    backgroundColor: ["rgba(235, 22, 22, .7)", "rgba(235, 22, 22, .6)", "rgba(235, 22, 22, .5)", "rgba(235, 22, 22, .4)", "rgba(235, 22, 22, .3)"],
+                    data: [55, 49, 44, 24, 15],
+                },
+            ],
         },
         options: {
-            responsive: true
-        }
+            responsive: true,
+        },
     });
-
 
     // Pie Chart
     var ctx5 = $("#pie-chart").get(0).getContext("2d");
@@ -168,22 +163,17 @@
         type: "pie",
         data: {
             labels: ["Italy", "France", "Spain", "USA", "Argentina"],
-            datasets: [{
-                backgroundColor: [
-                    "rgba(235, 22, 22, .7)",
-                    "rgba(235, 22, 22, .6)",
-                    "rgba(235, 22, 22, .5)",
-                    "rgba(235, 22, 22, .4)",
-                    "rgba(235, 22, 22, .3)"
-                ],
-                data: [55, 49, 44, 24, 15]
-            }]
+            datasets: [
+                {
+                    backgroundColor: ["rgba(235, 22, 22, .7)", "rgba(235, 22, 22, .6)", "rgba(235, 22, 22, .5)", "rgba(235, 22, 22, .4)", "rgba(235, 22, 22, .3)"],
+                    data: [55, 49, 44, 24, 15],
+                },
+            ],
         },
         options: {
-            responsive: true
-        }
+            responsive: true,
+        },
     });
-
 
     // Doughnut Chart
     var ctx6 = $("#doughnut-chart").get(0).getContext("2d");
@@ -191,22 +181,15 @@
         type: "doughnut",
         data: {
             labels: ["Italy", "France", "Spain", "USA", "Argentina"],
-            datasets: [{
-                backgroundColor: [
-                    "rgba(235, 22, 22, .7)",
-                    "rgba(235, 22, 22, .6)",
-                    "rgba(235, 22, 22, .5)",
-                    "rgba(235, 22, 22, .4)",
-                    "rgba(235, 22, 22, .3)"
-                ],
-                data: [55, 49, 44, 24, 15]
-            }]
+            datasets: [
+                {
+                    backgroundColor: ["rgba(235, 22, 22, .7)", "rgba(235, 22, 22, .6)", "rgba(235, 22, 22, .5)", "rgba(235, 22, 22, .4)", "rgba(235, 22, 22, .3)"],
+                    data: [55, 49, 44, 24, 15],
+                },
+            ],
         },
         options: {
-            responsive: true
-        }
+            responsive: true,
+        },
     });
-
-    
 })(jQuery);
-
