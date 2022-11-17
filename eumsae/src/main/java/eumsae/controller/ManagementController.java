fba1ebec7908e169f6f5c5b04754f76296ccee90
Choe-gitae <cgtcsg@naver.com>
2022-11-18 01:25:58 +0900

    insertLp 수정

--- eumsae/src/main/java/eumsae/controller/ManagementController.java
+++ eumsae/src/main/java/eumsae/controller/ManagementController.java
@@ -1,9 +1,39 @@
 package eumsae.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import eumsae.model.LpVO;
+import eumsae.service.lpinfoService;
+import eumsae.service.lpscanService;
 
 @Controller
+@RequestMapping(value = "/management")
 public class ManagementController {
+	
+	@Autowired
+	private lpinfoService lpinfoService;
 
+	@Autowired
+	private lpscanService lpscanService;
+	
+	// LP 정보 입력
+		@RequestMapping(value = "/insertLp")
+		public String insertLp(LpVO vo, Model m) {
+			int result = lpinfoService.insertLpInfo(vo);
+			if (result == 1) {
+				int result2 = lpscanService.insertLp(vo);
+				if (result2 == 1) {
+					m.addAttribute("result", vo);
+					return "/front/insertSuccess";
+				} else {
+					return "/front/error";
+				}
+			} else {
+				return "/front/error";
+			}
+		}
 	
 }
