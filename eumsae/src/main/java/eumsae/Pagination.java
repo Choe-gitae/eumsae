503608ccf751cdbcab852afa9cf332b2c52d90e9
Choe Gitae <cgtcsg@naver.com>
2022-11-22 18:25:51 +0900

    pagination

--- eumsae/src/main/java/eumsae/Pagination.java
+++ eumsae/src/main/java/eumsae/Pagination.java
@@ -6,7 +6,6 @@ public class Pagination {
 	   private int pageNum;     	// 현재 페이지(current)
 	   private int pageNavSize;  	// 페이지 보여줄 단위(사이즈)
 	   private long totalRecord;   	// 총 게시물(레코드) 갯수
-	   private long startRowIndex;  // 게시물의 시작번호(거꾸로시작)
 	   private int firstPageNo;   	// 페이지의 시작번호
 	   private int lastPageNo;      // 페이지의 마지막번호
 	   private int startRow;		// select 시작행
@@ -18,41 +17,32 @@ public class Pagination {
 	   private boolean hasLastPageNav;      // 마지막페이지
 	   
 	   
-	   public Pagination() {
-	      this.pageSize = 10;
+	   public Pagination(int pageNum, long totalRecord, int pageSize,  int pageNavSize) {
+	      this.pageNum = pageNum;
 	      this.totalRecord = totalRecord;
 	      this.pageSize = pageSize;
 	      this.pageNavSize = pageNavSize;
-
-	      if (this.pageNum < 1) {
-	         this.pageNum=1;
-	      }
-
-	      if (this.pageNavSize < 1) {
-	         this.pageSize=10;
-	      }
-	      this.totalRecord = totalRecord;
 	      calculation();
 	   }
 
 	   private void calculation() {
 	      // 총 페이지 수 계산
-	      totalPage = (int) ( (totalRecord - 1) / pageSize + 1);
-	      // 
+	      totalPage = (int) Math.ceil(totalRecord / (float)pageSize);
+	      // 현재 페이지가 총페이지 보다 커지면 현재페이지 총페이지로 제한
 	      if (pageNum > totalPage) pageNum = totalPage;
-
 	      // 현재 페이지번호로 페이지 링크 블럭의 시작페이지번호와 마지막페이지번호 계산
-	      this.firstPageNo = (((int) Math.ceil((float)this.pageNum / (float)this.pageNavSize))-1)*this.pageNavSize + 1;
-	      this.lastPageNo = this.firstPageNo + this.pageNavSize - 1;
-	      if (this.lastPageNo > this.totalPage) 
-	         this.lastPageNo = this.totalPage;
+	      firstPageNo = ( ( (int)Math.ceil((float)pageNum / (float)pageNavSize) ) -1 ) * pageNavSize +1;
+	      lastPageNo = firstPageNo + pageNavSize - 1;
+	      if (lastPageNo > totalPage) 
+	         lastPageNo = totalPage;
+	      // DB에서 검색해 올 시작로우 엔드로우 계산
+	      int startRow = (pageNum * pageSize) - (pageSize -1);
+	      int endRow = pageNum * pageSize;
 	      
-	      this.startRowIndex = this.totalRecord - ((this.pageNum-1) * this.pageSize);
-
-	      hasPrevPageNav =  this.firstPageNo == 1 ? false : true; 
-	      hasNextPageNav =(this.lastPageNo * this.pageSize) < this.totalRecord;
-	      
-	      hasFirstPageNav =  (this.pageNum > 1 && this.pageNavSize < this.pageNum); 
-	      hasLastPageNav = (this.pageNum < this.totalPage && this.lastPageNo + 1 < this.totalPage );       
+	      // 이전, 다음, 첫페이지, 끝페이지 표시여부 true,false
+	      hasPrevPageNav = firstPageNo == 1 ? false : true; 
+	      hasNextPageNav = (lastPageNo * pageSize) < totalRecord;
+	      hasFirstPageNav = (pageNum > 1 && pageNavSize < pageNum); 
+	      hasLastPageNav = (pageNum < totalPage && lastPageNo + 1 < totalPage );       
 	   }
 	}
