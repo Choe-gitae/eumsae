88bfaa86c48b18a6692a1366c93a47fde4fab54d
NightSeaBlue <nun9017@naver.com>
2022-11-24 20:18:51 +0900

    ?

--- eumsae/src/main/java/eumsae/controller/ShopController.java
+++ eumsae/src/main/java/eumsae/controller/ShopController.java
@@ -8,8 +8,10 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import eumsae.model.LpVO;
+import eumsae.service.CustomerService;
 import eumsae.service.LpService;
 
 @Controller
@@ -18,6 +20,9 @@ public class ShopController {
 
 	@Autowired
 	LpService lpService;
+	
+	@Autowired
+	CustomerService cService;
 
 	// 선택한 URL 로 이동
 	@RequestMapping(value = "/{url}")
